b49ca6c2e449c0637f9a584f090c1f0b0c72a748
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-02-25 22:29:11 +0630

    capabilities

---
+++ src/components/home/Capabilities.vue
@@ -0,0 +1,82 @@
+<template>
+  <section>
+    <div class="wrapper">
+      <h1>Our Capabilities</h1>
+      <div class="grid">
+        <div class="item">
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+              <path
+                d="M232.5 5.171C247.4-1.718 264.6-1.718 279.5 5.171L498.1 106.2C506.6 110.1 512 118.6 512 127.1C512 137.3 506.6 145.8 498.1 149.8L279.5 250.8C264.6 257.7 247.4 257.7 232.5 250.8L13.93 149.8C5.438 145.8 0 137.3 0 127.1C0 118.6 5.437 110.1 13.93 106.2L232.5 5.171zM498.1 234.2C506.6 238.1 512 246.6 512 255.1C512 265.3 506.6 273.8 498.1 277.8L279.5 378.8C264.6 385.7 247.4 385.7 232.5 378.8L13.93 277.8C5.438 273.8 0 265.3 0 255.1C0 246.6 5.437 238.1 13.93 234.2L67.13 209.6L219.1 279.8C242.5 290.7 269.5 290.7 292.9 279.8L444.9 209.6L498.1 234.2zM292.9 407.8L444.9 337.6L498.1 362.2C506.6 366.1 512 374.6 512 383.1C512 393.3 506.6 401.8 498.1 405.8L279.5 506.8C264.6 513.7 247.4 513.7 232.5 506.8L13.93 405.8C5.438 401.8 0 393.3 0 383.1C0 374.6 5.437 366.1 13.93 362.2L67.13 337.6L219.1 407.8C242.5 418.7 269.5 418.7 292.9 407.8V407.8z"
+              />
+            </svg>
+          </span>
+          <strong>Branding</strong>
+          <p>
+            It’s more than just a logo, font, and colours! Without strong
+            branding that lays the foundation for your marketing activities,
+            you'll just be more white noise to the average consumer.
+          </p>
+        </div>
+        <div class="item">
+          <i class="fa-solid fa-layer-group"></i>
+        </div>
+        <div class="item">
+          <i class="fa-solid fa-layer-group"></i>
+        </div>
+        <div class="item">
+          <i class="fa-solid fa-layer-group"></i>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<style scoped>
+section {
+  padding: 10rem 0;
+}
+
+.wrapper {
+  max-width: var(--max-width);
+  margin: 0 auto;
+}
+
+h1 {
+  font-weight: bolder;
+  font-size: 75px;
+  text-align: center;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(18rem, 1fr));
+  grid-template-rows: repeat(2,min-content);
+  grid-gap: 2rem;
+}
+
+.item {
+  border: 1px solid #ccc;
+  display: grid;
+  grid-template-columns: 58px 1fr;
+}
+
+.item span {
+  display: block;
+  width: 58px;
+  height: 58px;
+  grid-column: 1 / 1;
+}
+
+.item strong {
+    grid-column: 2 / -1;
+    grid-row: 1 / 2;
+}
+
+.item p {
+    grid-column: 2 / -1;
+    grid-row: 3 / -1;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,18 @@
 <script setup>
+import Capabilities from "./../components/home/Capabilities.vue";
 </script>
 
 <template>
-<section>
+  <section>
     <div class="video-bg">
-        <video src="https://midas-api.venuslab.co/storage/video_thumbnails/00cdb98d-5d8b-4c68-8bb6-c898a0eec529.mp4" autoplay loop></video>
+      <video
+        src="https://midas-api.venuslab.co/storage/video_thumbnails/00cdb98d-5d8b-4c68-8bb6-c898a0eec529.mp4"
+        autoplay
+        loop
+      ></video>
     </div>
-</section>
+  </section>
+  <Capabilities />
 </template>
 
 <style scoped>
@@ -14,17 +20,14 @@ section {
   height: 100vh;
 }
 .video-bg {
-    width: 100%;
-      height: 100vh;
+  width: 100%;
+  height: 100vh;
 }
 
-video{
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    object-position: top;
+video {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: top;
 }
-
-
-
 </style>
