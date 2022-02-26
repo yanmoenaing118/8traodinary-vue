b06d3fee5eed6f8b32464b941776ed9393949d88
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-02-26 11:28:32 +0630

    footer done

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,3 +1,29 @@
+<script setup>
+import FooterWrapper from "./footer/FooterWrapper.vue";
+</script>
+
 <template>
-    <footer>App footer</footer>
+    <footer>
+        <FooterWrapper />
+        <div class="copy">Copyrigh MidasCratives &copy; 2022</div>
+    </footer>
 </template>
+
+
+<style scoped>
+footer {
+    padding: 5vw 0 0 0;
+    background-color: #101213;;
+    color: #fff;
+}
+
+.copy {
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    box-shadow: 0 -1px 1px rgba(255,255,255,0.3);
+
+}
+</style>
---
+++ src/components/layout/footer/FooterWrapper.vue
@@ -0,0 +1,101 @@
+<template>
+<div class="wrapper">
+    <div class="logo">
+        <img src="https://midas.venuslab.co/logo.png" alt="Midas Creative Footer Logo">
+    </div>
+
+    <nav>
+        <div>
+            <h3>Office</h3>
+            <ul>
+                <li>
+                    <address>
+                        <strong>Yangon</strong>
+                        <div>
+                            Fancy House Myanmar 56B <br> Kabar Aye Pagoda Road Yangon 11081
+                        </div>
+                    </address>
+                </li>
+                <li>
+                    <div>
+                        <strong>Singapore</strong>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Contact</h3>
+            <ul>
+                <li>
+                    <a href="#">example@midascrative.com</a>
+                </li>
+                <li>
+                    <a href="#">+959 7680 47177</a>
+                </li>
+            
+            </ul>
+        </div>
+
+        <div>
+            <h3>Others</h3>
+            <ul>
+                <li>Other One</li>
+                <li>Other Two</li>
+                <li>Other Three</li>
+            </ul>
+        </div>
+    </nav>
+</div>
+</template>
+
+<style scoped>
+.wrapper {
+    width: 100%;
+    width: var(--max-width);
+    margin: 0 auto;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0 0 5vw 0;
+}
+
+nav {
+    margin-left: auto;
+    display: flex;
+    gap: var(--space-11);
+}
+
+h3 {
+    display: inline-block;
+}
+
+h3::after {
+    content: "";
+    display: table;
+    height: 1px;
+    width: 150%;
+    background-color: #fff;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+
+}
+
+ul {
+    list-style-type: none;
+}
+
+address div {
+    padding-left: var(--space-4);
+}
+
+li {
+    margin: var(--space-8) 0;
+}
+
+strong {
+    font-size: 18px;
+}
+
+</style>
